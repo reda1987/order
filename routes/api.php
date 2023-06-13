1432bd7313c97cd1647e6614b3b90286f65dcc77
CASHCALL\mohamedr <mohamed.ribrahim@cashcall.com.eg>
2023-06-13 23:37:10 +0300

    3|resource_management

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,14 @@ Route::group([
 ], function ($router) {    
     Route::post('/register', [AuthController::class, 'register']);  
     Route::post('/login', [AuthController::class, 'login']);
+});
+
+
+Route::group([
+    'middleware' => 'api'    
+], function ($router) {
+    Route::post('/product', [ProductsController::class, 'store']);
+    Route::get('/product/{product}', [ProductsController::class, 'show']);
+    Route::put('/product/{id}', [ProductsController::class, 'update']);
+    Route::delete('/product/{id}', [ProductsController::class, 'destroy']); 
 });
